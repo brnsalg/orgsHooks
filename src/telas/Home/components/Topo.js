fec6831e6ee6e93f387ce2e8851bde822b56ac76
brnsalg <salgado.bhn@gmail.com>
2023-04-04 23:38:07 -0300

    wip

---
+++ src/telas/Home/components/Topo.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Text, View, Image, StyleSheet } from 'react-native';
+
+import { carregaTopo } from '../../../services/carregaDados';
+import logo from '../../../assets/logo.png';
+
+class Topo extends React.Component {
+  state = {
+    topo: {
+      boasVindas: '',
+      legenda: '',
+    },
+    lista: [],
+  };
+
+  atualizaTopo() {
+    const retorno = carregaTopo();
+    this.setState({ topo: retorno });
+  }
+
+  componentDidMount() {
+    this.atualizaTopo();
+  }
+
+  render() {
+    return (
+      <View style={styles.topo}>
+        <Image style={styles.imagem} source={logo} />
+        <Text style={styles.boasVindas}>{this.state.topo.boasVindas}</Text>
+        <Text style={styles.legenda}>{this.state.topo.legenda}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  topo: {
+    backgroundColor: '#f6f6f6',
+    padding: 16,
+  },
+  imagem: {
+    width: 70,
+    height: 28,
+  },
+  boasVindas: {
+    marginTop: 24,
+    fontSize: 26,
+    lineHeight: 42,
+    fontWeight: 'bold',
+  },
+  legenda: {
+    fontSize: 16,
+    lineHeight: 26,
+  },
+});
+
+export default Topo;
